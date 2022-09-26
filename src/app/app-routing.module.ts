66a41b35e9faddc579441600b2d98c3e19a861d6
FABIAN <stehen894@gmail.com>
2022-09-26 01:26:19 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddContactComponent } from './component/add-contact/add-contact.component';
+import { ContactManagerComponent } from './component/contact-manager/contact-manager.component';
+import { EditContactComponent } from './component/edit-contact/edit-contact.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import { ViewContactComponent } from './component/view-contact/view-contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : '', redirectTo : '/contacts/admin', pathMatch: 'full'},
+  { path : 'contacts/admin' , component: ContactManagerComponent},
+  { path : 'contacts/add' , component: AddContactComponent},
+  { path : 'contacts/edit/:contactId' , component: EditContactComponent},
+  { path : 'contacts/view/:contactId' , component: ViewContactComponent},
+  { path : '**' , component: PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { ContactManagerComponent } from './component/contact-manager/contact-manager.component';
+import { AddContactComponent } from './component/add-contact/add-contact.component';
+import { EditContactComponent } from './component/edit-contact/edit-contact.component';
+import { ViewContactComponent } from './component/view-contact/view-contact.component';
+import { SpinnerComponent } from './component/spinner/spinner.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContactManagerComponent,
+    AddContactComponent,
+    EditContactComponent,
+    ViewContactComponent,
+    SpinnerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
